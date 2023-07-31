e3f750713fa17c9ed5c4292e7fad7922abaf955f
Denis Solonenko <denis.solonenko@gmail.com>
2023-07-31 21:39:25 +1000

    Calculate file hashes

---
+++ lib/src/file_hash.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:chunked_stream/chunked_stream.dart';
+import 'package:crclib/catalog.dart';
+import 'package:crclib/crclib.dart';
+import 'package:crypto/crypto.dart';
+
+import 'package:convert/convert.dart';
+
+class FileHash {
+  final String crc;
+  final String md5;
+  final String sha1;
+
+  FileHash({
+    required this.crc,
+    required this.md5,
+    required this.sha1,
+  });
+}
+
+Future<FileHash?> getFileHash(String filePath) async {
+  final file = File(filePath);
+  if (!file.existsSync()) return null;
+  try {
+    final stream = file.openRead();
+    final bufferedStream = bufferChunkedStream(stream, bufferSize: 128 * 1024);
+    // ignore: deprecated_member_use
+    final iterator = ChunkedStreamIterator(bufferedStream);
+    try {
+      final md5out = AccumulatorSink<Digest>();
+      final md5in = md5.startChunkedConversion(md5out);
+      final sha1out = AccumulatorSink<Digest>();
+      final sha1in = sha1.startChunkedConversion(sha1out);
+      final crcout = AccumulatorSink<CrcValue>();
+      final crcin = Crc32().startChunkedConversion(crcout);
+
+      while (true) {
+        final chunk = await iterator.read(32 * 1024);
+        if (chunk.isEmpty) {
+          break;
+        }
+        md5in.add(chunk);
+        sha1in.add(chunk);
+        crcin.add(chunk);
+      }
+      md5in.close();
+      sha1in.close();
+      crcin.close();
+
+      final md5hash = md5out.events.single.toString().toUpperCase();
+      final sha1hash = sha1out.events.single.toString().toUpperCase();
+      final crc32hash = crcout.events.single.toRadixString(16).toUpperCase();
+
+      return FileHash(
+        crc: crc32hash,
+        md5: md5hash,
+        sha1: sha1hash,
+      );
+    } finally {
+      await iterator.cancel();
+    }
+  } catch (exception) {
+    return null;
+  }
+}
--- test/screenscraper_test.dart
+++ test/screenscraper_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:screenscraper/screenscraper.dart';
+import 'package:screenscraper/src/file_hash.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -21,10 +22,20 @@ void main() {
     });
 
     test('Game Info', () async {
-      final game = await api.gameInfo(
-          GameInfoRequest.romByHash(systemeid: "1", romnom: "Sonic The Hedgehog 2 (World).zip", crc: "50ABC90A"));
+      final game = await api.gameInfo(GameInfoRequest.romByHash(
+        systemeid: "1",
+        romnom: "Sonic The Hedgehog 2 (World).zip",
+        crc: "50ABC90A",
+      ));
       expect(game.id, equals("3"));
       expect(game.romid, equals("12100"));
+    });
+
+    test('File hash', () async {
+      final hash = await getFileHash("WarioWare, Inc. - Mega Microgame\$! (USA).gba");
+      expect(hash!.crc, equals("785D8B8C"));
+      expect(hash.md5, equals("A2D26DC774CEC9A0B47388A5DD727B03"));
+      expect(hash.sha1, equals("3F556448D290FA5406D6ED367FEE16CC02387AD3"));
     });
   });
 }
